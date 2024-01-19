6303e1d3206eebafc31167e25b275efcd40b2d80
jrferguson <1q2w3e4r5t6y>
2024-01-19 09:13:20 +0000

    added list of ministries

--- src/app/components/pages/ministries/content/content.component.ts
+++ src/app/components/pages/ministries/content/content.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { MinistryHelperService } from 'src/app/components/helper/ministry/ministry-helper.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent extends MinistryHelperService {
+export class ContentComponent {
+  @Input() ministries: any;
+
+  constructor() { }
+
 }
