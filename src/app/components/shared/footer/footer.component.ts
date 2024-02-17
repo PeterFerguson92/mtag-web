56c7c2d7e063e50cd17a0b1e82536d063e62a93e
jrferguson <1q2w3e4r5t6y>
2024-02-17 22:08:09 +0000

    footer fix

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import data from "../../data/footerlinks.json";
-import { BlogHelperService } from '../../helper/blog/blog-helper.service';
+import { Component, OnInit } from '@angular/core';
+import data from '../../data/footerlinks.json';
+import { ApiService } from '../../data/service/api.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent extends BlogHelperService {
-  public links = data; 
+export class FooterComponent implements OnInit {
+  public links = data;
+  description = '';
+  content1 = '';
+  content2 = '';
+  address = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getResource('/homepage/details').subscribe(
+      (result: any) => {
+        if (result.status === 'success')
+        {
+          this.description = result.result[0].description;
+          this.content1 = result.result[0].info_content_1;
+          this.content2 = result.result[0].info_content_2;
+          this.address = result.result[0].address;
+
+        }
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
 }
